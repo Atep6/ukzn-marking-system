1c9eaf00e7db3283f282b03f3730fd4cbaa6607b
Dev Null <devnull@example.com>
2025-10-21 00:04:22 +0200

    chore: move ts-jest config into transform; remove deprecated globals

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  rootDir: '.',
+  roots: ['<rootDir>/tests/unit'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      require.resolve('ts-jest'),
+      {
+        tsconfig: 'tsconfig.json',
+        isolatedModules: false
+      }
+    ]
+  },
+  testMatch: ['**/tests/unit/**/*.test.ts', '**/tests/unit/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/tests/e2e/'],
+  transformIgnorePatterns: ['/node_modules/']
+};
